8f09808c783245443d55dcaaa47e0366ad6f9390
manonbrz <manon.lebarazer@ynov.com>
2025-10-10 10:25:03 +0200

    add documentation

--- src/eshop.services/basket/Basket.API/Features/Baskets/Commands/DeleteBasketItem/DeleteBasketItemCommandValidator.cs
+++ src/eshop.services/basket/Basket.API/Features/Baskets/Commands/DeleteBasketItem/DeleteBasketItemCommandValidator.cs
@@ -3,8 +3,14 @@ using FluentValidation;
 
 namespace Basket.API.Features.Baskets.Commands.DeleteBasketItem;
 
+/// <summary>
+/// Validator for the <see cref="DeleteBasketItemCommand"/>.
+/// </summary>
 public class DeleteBasketItemCommandValidator : AbstractValidator<DeleteBasketItemCommand>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeleteBasketItemCommandValidator"/> class.
+    /// </summary>
     public DeleteBasketItemCommandValidator()
     {
         RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
