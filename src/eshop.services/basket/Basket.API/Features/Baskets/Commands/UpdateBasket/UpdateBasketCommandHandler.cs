10860df472b76284fac3145881422e0cd94ef44a
AlexandreBudan <LuckyNightm4re@gmail.com>
2025-10-10 10:30:45 +0200

    add xml doc

--- src/eshop.services/basket/Basket.API/Features/Baskets/Commands/UpdateBasket/UpdateBasketCommandHandler.cs
+++ src/eshop.services/basket/Basket.API/Features/Baskets/Commands/UpdateBasket/UpdateBasketCommandHandler.cs
@@ -3,8 +3,17 @@ using Basket.API.Data.Repositories;
 
 namespace Basket.API.Features.Baskets.Commands.UpdateBasket;
 
+/// <summary>
+/// Handles the <see cref="UpdateBasketCommand"/> to update a user's shopping basket.
+/// </summary>
 public class UpdateBasketCommandHandler(IBasketRepository repository) : ICommandHandler<UpdateBasketCommand, UpdateBasketCommandResult>
 {
+    /// <summary>
+    /// Handles the <see cref="UpdateBasketCommand"/> and updates the basket.
+    /// </summary>
+    /// <param name="command">The command to handle.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The result of the command.</returns>
     public async Task<UpdateBasketCommandResult> Handle(UpdateBasketCommand command, CancellationToken cancellationToken)
     {
         var basket = await repository.GetBasketByUserNameAsync(command.UserName, cancellationToken);
--- src/eshop.services/basket/Basket.API/Features/Baskets/Commands/UpdateBasket/UpdateBasketCommandValidator.cs
+++ src/eshop.services/basket/Basket.API/Features/Baskets/Commands/UpdateBasket/UpdateBasketCommandValidator.cs
@@ -2,8 +2,14 @@ using FluentValidation;
 
 namespace Basket.API.Features.Baskets.Commands.UpdateBasket;
 
+/// <summary>
+/// Validator for the <see cref="UpdateBasketCommand"/>.
+/// </summary>
 public class UpdateBasketCommandValidator : AbstractValidator<UpdateBasketCommand>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpdateBasketCommandValidator"/> class.
+    /// </summary>
     public UpdateBasketCommandValidator()
     {
         RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
