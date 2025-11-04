f8d05a375f5c5f16c9e1f09a6f002c00700ac31e
AlexandreBudan <LuckyNightm4re@gmail.com>
2025-11-04 14:22:58 +0100

    Add update and cancelled mail for orders

--- src/eshop.services/ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ src/eshop.services/ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -10,6 +10,15 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder;
 
 public class DeleteOrderCommandHandler(IOrderingDbContext orderingDbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderCommandResult>
 {
+    /// <summary>
+    /// Handles the operation for deleting an order based on the provided command.
+    /// </summary>
+    /// <param name="request">The command containing the order identifier to delete.</param>
+    /// <param name="cancellationToken">Token to observe while waiting for the task to complete.</param>
+    /// <returns>A <see cref="DeleteOrderCommandResult"/> containing the result of the delete operation.</returns>
+    /// <exception cref="OrderNotFoundException">
+    /// Thrown when an order with the specified identifier is not found.
+    /// </exception>
     public async Task<DeleteOrderCommandResult> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
     {
         var orderId = OrderId.Of(request.OrderId);
